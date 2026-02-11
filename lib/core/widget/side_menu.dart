87e9883cf1a453f585d6c1ad70012c190a132330
DzaakhIdn <dzaakhidn@gmail.com>
2026-02-11 14:47:21 +0700

    popp

--- lib/core/widget/side_menu.dart
+++ lib/core/widget/side_menu.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
 import 'package:khataman_app/core/style/app_colors.dart';
 import 'package:khataman_app/core/widget/models/side_menu_assets.dart';
+import 'package:khataman_app/features/auth/provider/auth_provider.dart';
 import 'info_profile.dart';
 import '../style/icon_sets.dart';
 import 'side_menu_tile.dart';
 
-class SideMenu extends StatefulWidget {
+class SideMenu extends ConsumerStatefulWidget {
   static const routeName = '/side_bar';
   const SideMenu({super.key});
 
   @override
-  State<SideMenu> createState() => _SideMenuState();
+  ConsumerState<SideMenu> createState() => _SideMenuState();
 }
 
-class _SideMenuState extends State<SideMenu> {
+class _SideMenuState extends ConsumerState<SideMenu> {
+  SideMenuAssets? selectedMenu;
+
+  void _handleMenuNavigation(SideMenuAssets menu) {
+    switch (menu.title) {
+      case "Home":
+        // context.go('/home');
+        break;
+      case "History":
+        // TODO: Navigate to history
+        break;
+      case "Settings":
+        // TODO: Navigate to settings
+        break;
+      case "About":
+        // TODO: Navigate to about
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final authState = ref.watch(authProvider);
+
+    final userName = authState.userId != null ? "User" : "Guest";
+    final userProfession = "not set";
+
     return Scaffold(
       body: Container(
         width: 288,
@@ -25,14 +53,15 @@ class _SideMenuState extends State<SideMenu> {
         color: AppColors.light.secondary,
         child: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              infoProfile(name: "Ayam", profesion: "not set"),
+              infoProfile(name: userName, profesion: userProfession),
               Padding(
                 padding: const EdgeInsets.only(left: 24, top: 32, bottom: 16),
                 child: Text(
                   "Browse".toUpperCase(),
                   style: GoogleFonts.raleway(
-                    textStyle: TextStyle(
+                    textStyle: const TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 17,
@@ -41,8 +70,55 @@ class _SideMenuState extends State<SideMenu> {
                 ),
               ),
               ...asetIcon.map(
-                (menu) =>
-                    sideMenuTile(menu: menu, isActive: false, press: () {}),
+                (menu) => sideMenuTile(
+                  menu: menu,
+                  isActive: selectedMenu == menu,
+                  press: () {
+                    setState(() {
+                      selectedMenu = menu;
+                    });
+                    _handleMenuNavigation(menu);
+                  },
+                ),
+              ),
+
+              const Spacer(),
+
+              // Logout button
+              Padding(
+                padding: const EdgeInsets.all(24),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton.icon(
+                    onPressed: () async {
+                      // Logout user
+                      await ref.read(authProvider.notifier).logout();
+                      if (context.mounted) {
+                        context.goNamed('/signin');
+                      }
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Colors.white),
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    label: Text(
+                      'Logout',
+                      style: GoogleFonts.raleway(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
--- lib/core/widget/side_menu_tile.dart
+++ lib/core/widget/side_menu_tile.dart
@@ -24,19 +24,35 @@ class sideMenuTile extends StatelessWidget {
           padding: EdgeInsets.only(left: 24),
           child: Divider(color: Colors.white24, height: 1),
         ),
-        ListTile(
-          onTap: press,
-          leading: SizedBox(
-            height: 34,
-            width: 34,
-            child: Iconify(menu.icon, color: Colors.black38),
-          ),
-          title: Text(
-            menu.title,
-            style: GoogleFonts.raleway(
-              textStyle: TextStyle(color: AppColors.light.textPrimary),
+        Stack(
+          children: [
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 300),
+              height: 56,
+              width: isActive ? 288 : 0,
+              left: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-          ),
+            ListTile(
+              onTap: press,
+              leading: SizedBox(
+                height: 34,
+                width: 34,
+                child: Iconify(menu.icon, color: Colors.black38),
+              ),
+              title: Text(
+                menu.title,
+                style: GoogleFonts.raleway(
+                  textStyle: TextStyle(color: AppColors.light.textPrimary),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
